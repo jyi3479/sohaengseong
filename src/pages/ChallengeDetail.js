9f29ba5ae956c7900da7fcfb771bf4efef901d62
aaanjini <zini6633@gmail.com>
2022-02-28 23:28:28 +0900

    챌린지 소개페이지 60% 작업중

--- src/pages/ChallengeDetail.js
+++ src/pages/ChallengeDetail.js
@@ -1,9 +1,83 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { history } from "../redux/configureStore";
+import {Grid} from "../elements/index";
+import { actionCreators as challengeAction } from "../redux/modules/challenge";
 
 const ChallengeDetail = (props) => {
+    const dispatch = useDispatch();
+    const challengeId = props.match.params.challengeId;
+    const target = useSelector(state => state.challenge.target);
+    const  tagList = target.tagName;
+    const members = target.members;
+
+    const confirm = () => {
+        window.confirm("다른 사람들을 위해 신중하게 선택하세요! 확인을 클릭 시 챌린지에 입장합니다");
+        if(confirm){
+            console.log("확인누름");
+            //dispatch(challengeAction.joinChallengeDB(challengeId));
+            //history.push(``); //상세페이지 (멤버전용)으로 이동
+        }
+    };
+    const prompt = () => {
+        const pwd = window.prompt("비밀번호를 입력하세요");        
+        console.log(pwd);
+        //dispatch(challengeAction.joinChallengeDB(challengeId));
+        //history.push(``); //상세페이지 (멤버전용)으로 이동        
+    } 
+
+    
+    //특정 챌린지 1개 조회하기
+    // React.useEffect(()=>{
+    //     dispatch(challengeAction.getOneChallengeDB(challengeId));
+    // },[]);
+
+    console.log("타겟",target, challengeId);
     return(
-        <>챌린지소개페이지</>
+        <>
+            <Grid>
+                <Grid width="200px"> 
+                    <img src={target.challengeImage}/>
+                </Grid>
+                <Grid>
+                    <p>{target.title}</p>
+                    <p>{target.content}</p>
+                    <Grid>
+                        {tagList.map((el, i) => {
+                            return <Tag key={i}>{el}</Tag>;
+                        })}
+                    </Grid>
+                    <p>{target.currentMember}/{target.maxMember}</p>
+                    <Grid>
+                        {members.map((el, i) => {
+                            return (
+                                <div style={{display:"inline-block"}} key={el.userId}>
+                                    <img src={el.profileImage} style={{width:"30px", height:"30px",borderRadius:"50%"}}/>
+                                </div>                                
+                            );
+                        })}
+                    </Grid>
+                </Grid>
+                {target.isPrivate? (                    
+                    <button type="button" onClick={
+                        prompt
+                    }>챌린지 참여하기</button>                    
+                ):(
+                    <button type="button" onClick={
+                        confirm
+                    }>챌린지 참여하기</button>
+                )}
+                
+            </Grid>
+        </>
     );
 };
+
+const Tag = styled.p`
+  display: inline-block;
+  margin: 0;
+  margin-right: 5px;
+`;
 
 export default ChallengeDetail;
--- src/redux/modules/challenge.js
+++ src/redux/modules/challenge.js
@@ -2,9 +2,10 @@ import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 
 import axios from "axios";
-import _ from "lodash";
+import { challengeApis } from "../../shared/apis";
 
 const GET_CHALLENGE = "GET_CHALLENGE";
+const TARGET_CHALLENGE = "TARGET_CHALLENGE";
 const GET_CATEGORY = "GET_CATEGORY";
 const GET_CATEGORY_LIST = "GET_CATEGORY_LIST";
 const ADD_CHALLENGE = "ADD_CHALLENGE";
@@ -12,6 +13,7 @@ const EDIT_CHALLENGE = "ADD_CHALLENGE";
 const DELETE_CHALLENGE = "DELETE_CHALLENGE";
 
 const getChallenge = createAction(GET_CHALLENGE, (challenge_list)=>({challenge_list}));
+const targetChallenge = createAction(TARGET_CHALLENGE, (target) => ({target}));
 const addChallenge = createAction(ADD_CHALLENGE, (challenge)=>({challenge}));
 const editChallenge = createAction(EDIT_CHALLENGE, (challengeId, challenge) =>({challengeId, challenge}));
 const deleteChallenge = createAction(DELETE_CHALLENGE, (challengeId,) =>({challengeId}));
@@ -200,13 +202,65 @@ const initialState = {
             }
         ],
     
-    ]
+    ],
+    target:{
+        challengeId:"0",
+        title:"일상",
+        content:"우리의 운동조건은 열심히 하는 것 단 하나 입니다",   
+        category:"일상",
+        challengeImage:"http://www.readersnews.com/news/photo/201502/52827_9951_450.jpg",
+        maxMember:10,
+        currentMember:0,
+        startDate:"2022-02-28",
+        endDate:"2022-03-10",
+        isPrivate:true,
+        password:1234,
+        tagName:["해시태그1", "해시태그2", "해시태그3"],
+        members:[
+            {
+                userId:"0",
+                nickname:"닉네임1",
+                profileImage:"http://www.readersnews.com/news/photo/201502/52827_9951_450.jpg"
+            },
+            {
+                userId:"1",
+                nickname:"닉네임2",
+                profileImage:"http://www.readersnews.com/news/photo/201502/52827_9951_450.jpg"
+            },
+        ],
+        status:"ing"
+    },
 };
+
+const getOneChallengeDB = (challengeId) => {
+    return function (dispatch, getState, {history}) {
+        // challengeApis.getOneChallenge(challengeId)
+        // .then((res)=>{
+        //     console.log("특정 챌린지 조회",res);
+        // }).catch((err)=>{
+        //     console.log("특정 챌린지 조회 오류",err);
+        // });
+    };
+};
+
+const joinChallengeDB = (challengeId) => {
+    return function (dispatch, getState, {history}) {
+        // challengeApis.joinChallenge(challengeId)
+        // .then((res)=>{
+        //     console.log("챌린지 참여하기",res);
+        // }).catch((err)=>{
+        //     console.log("챌린지 참여하기 오류",err);
+        // });
+    };
+}
 
 
 export default handleActions ({
     [GET_CHALLENGE]: (state,action) => produce(state, (draft) => {
         draft.list = action.payload.challenge_list;
+    }),
+    [TARGET_CHALLENGE]:(state,action) => produce(state, (draft) => {
+        draft.target = action.payload.target;
     }),
     [GET_CATEGORY]: (state,action) => produce(state, (draft) => {
         draft.category = action.payload.category;
@@ -223,6 +277,8 @@ export default handleActions ({
 const actionCreators = { //액션 생성자 내보내기
     getChallenge,
     getCategory,
+    getOneChallengeDB,
+    joinChallengeDB
 };
 
 export {actionCreators};
