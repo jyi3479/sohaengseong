ba1f4333999f94d2dc3bdad9660ad02f85f7228a
aaanjini <zini6633@gmail.com>
2022-03-02 01:24:06 +0900

    네비 95% 진행중

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,23 +7,25 @@ import my from "../image/icons/ic_my@2x.png";
 
 const Footer = (props) => {
     return(
-        <Wrap>            
-            <div>
-                <a><img src={home}></img></a>
+        <Wrap>          
+            <a>
+                <img src={home}></img>
                 <p>홈</p>
-            </div>
-            <div>
-                <a><img src={create}></img></a>
+            </a>
+        
+            <a>
+                <img src={create}></img>
                 <p>개설하기</p>
-            </div>
-            <div>
-                <a><img src={chat}></img></a>
+            </a>            
+            <a>
+                <img src={chat}></img>
                 <p>채팅하기</p>
-            </div>
-            <div>
-                <a><img src={my}></img></a>
+            </a>
+        
+            <a>
+                <img src={my}></img>
                 <p>내 정보</p>
-            </div>            
+            </a>
         </Wrap>
     );
 };
@@ -38,30 +40,29 @@ const Wrap = styled.div`
     padding: 10px 20px;
     box-shadow: 0 -5px 6px 0 rgba(0, 0, 0, 0.04);
     box-sizing: border-box;
-    >ul {
-        display: flex;
-        justify-content: space-between;
-        margin: 0;
-        padding: 0;
-        li {
-            list-style: none;
+   
+    display: flex;
+    justify-content: space-between;
+    a {
+        list-style: none;
+        display: inline-block;
+        text-align: center;
+    
             display: inline-block;
-            text-align: center;
-            a {
-                display: inline-block;
-                height: 28px;
-            }
-            img {
-                width: 28px;
-            }
-            p {
-                color: #000;
-                font-size: 11px;
-                line-height: 20px;
-                margin: 0;
-            }
+            height: 28px;
+    
+        img {
+            width: 28px;
+        }
+        p {
+            color: #000;
+            font-size: 11px;
+            line-height: 20px;
+            margin: 0;
         }
     }
+    
+
 `;
 
 export default Footer;
