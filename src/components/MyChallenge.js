514064521163f138f9a3f72af8a9227f8504df7d
jyi3479 <jyi3479@naver.com>
2022-03-04 21:40:15 +0900

    마이페이지 뷰 40%

--- src/components/MyChallenge.js
+++ src/components/MyChallenge.js
@@ -4,6 +4,7 @@ import { history } from "../redux/configureStore";
 import { useSelector } from "react-redux";
 import { Grid } from "../elements";
 import Card from "./Card";
+import MypageCard from "./MypageCard";
 import { challengeApis } from "../shared/apis";
 
 const MyChallenge = (props) => {
@@ -29,8 +30,24 @@ const MyChallenge = (props) => {
   return (
     <>
       <Wrap>
-        <Grid>
-          <p>오늘의 챌린지</p>
+        <StatusContainer>
+          <Grid padding="14px">
+            <Grid center>
+              <p>진행중</p>
+              <p>{ing_list.length}</p>
+            </Grid>
+          </Grid>
+          <Grid padding="14px">
+            <Grid center>
+              <p>완료</p>
+              <p>{completed_list.length}</p>
+            </Grid>
+          </Grid>
+        </StatusContainer>
+        <Grid padding="24px 0px 0px">
+          <Title>오늘의 챌린지</Title>
+          <SubTitle>현재 진행중인 습관 챌린지</SubTitle>
+          <MypageCard></MypageCard>
           {ing_list.map((el, i) => {
             return (
               <Card
@@ -44,8 +61,9 @@ const MyChallenge = (props) => {
           })}
         </Grid>
 
-        <Grid>
-          <p>모집 중인 챌린지</p>
+        <Grid padding="24px 0px 0px">
+          <Title>입주민 모집중</Title>
+          <SubTitle>내가 개설한 챌린지</SubTitle>
           {before_list.map((el, i) => {
             return (
               <Card
@@ -59,8 +77,9 @@ const MyChallenge = (props) => {
           })}
         </Grid>
 
-        <Grid>
-          <p>지난 챌린지</p>
+        <Grid padding="24px 0px 0px">
+          <Title>지나온 행성들</Title>
+          <SubTitle>내가 진행했던 챌린지</SubTitle>
           {completed_list.map((el, i) => {
             // 지난 챌린지 중 성공일 경우
             if (el.status === "success") {
@@ -69,7 +88,7 @@ const MyChallenge = (props) => {
                   key={el.challengeId}
                   {...el}
                   _onClick={() => {
-                    history.push(`/member/${el.challengeId}`); // 멤버 전용 페이지로 이동
+                    history.push(`/challenge/${el.challengeId}`); //소개 페이지로 이동
                   }}
                   bg="orange"
                 ></Card>
@@ -81,7 +100,7 @@ const MyChallenge = (props) => {
                 key={el.challengeId}
                 {...el}
                 _onClick={() => {
-                  history.push(`/member/${el.challengeId}`); // 멤버 전용 페이지로 이동
+                  history.push(`/challenge/${el.challengeId}`); //소개 페이지로 이동
                 }}
                 bg="red"
               ></Card>
@@ -94,11 +113,41 @@ const MyChallenge = (props) => {
 };
 
 const Wrap = styled.div`
-  padding: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+const StatusContainer = styled.div`
+  display: flex;
+  width: 335px;
+  height: 69px;
+  border-radius: 8px;
+  background-color: #f7f7f7;
+  p:first-child {
+    font-size: 12px;
+    color: #808080;
+  }
+  p:last-child {
+    font-size: 16px;
+    font-weight: bold;
+    color: #000;
+  }
+`;
+
+const Title = styled.h3`
+  font-size: 18px;
+`;
+
+const SubTitle = styled.p`
+  opacity: 0.5;
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 1.69;
+  font-size: 13px;
+  color: #5f5f5f;
+  margin-bottom: 12px;
 `;
 
 export default MyChallenge;
--- src/components/MyLevel.js
+++ src/components/MyLevel.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import { Grid } from "../elements";
+import { Button, Grid, Image } from "../elements";
 import { challengeApis } from "../shared/apis";
 
 const MyLevel = (props) => {
@@ -10,25 +10,122 @@ const MyLevel = (props) => {
 
   return (
     <Wrap>
-      <Icon size={130} src={my_level.levelIcon} />
-      <p>
-        {my_level.nickname}
-        <span onClick={() => console.log("프로필 수정 페이지로 이동")}>⚙️</span>
-      </p>
-      <Grid is_flex>
-        <p>{my_level.levelName}/5</p>
-      </Grid>
-      <Progress my_level={my_level} />
+      <MyContainer>
+        <Grid is_flex padding="0px">
+          <div style={{ display: "flex" }}>
+            <div>
+              <Image shape="circle" size={40} />
+            </div>
+            <Grid padding="0px 0px 0px 9px">
+              <p
+                style={{
+                  fontSize: "14px",
+                  lineHeight: "1.57",
+                  letterSpacing: "-0.42px",
+                }}
+              >
+                {my_level.nickname}
+              </p>
+              <p style={{ fontSize: "12px", color: "#999999" }}>
+                {my_level.levelName}
+              </p>
+            </Grid>
+          </div>
+
+          <ProfileBtn onClick={() => console.log("프로필 수정 페이지로 이동")}>
+            편집
+          </ProfileBtn>
+        </Grid>
+        <Progress my_level={my_level} />
+      </MyContainer>
+      <MyPlanet>
+        {/* <TooltipBox>
+          <Tooltip>오늘 인증을 하면 레벨업!</Tooltip>
+        </TooltipBox> */}
+        <Icon size={92} src={my_level.levelIcon} />
+      </MyPlanet>
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
-  padding: 10px;
+  padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+const MyContainer = styled.div`
+  background-color: #ffffff;
+  width: 335px;
+  height: 99px;
+  border-radius: 10px;
+  padding: 14px;
+`;
+
+const ProfileBtn = styled.button`
+  width: 43px;
+  height: 29px;
+  margin: 0px 0px 16px;
+  border-radius: 6px;
+  border: solid 1px #ccc;
+  background-color: #fff;
+  color: #cccccc;
+  font-size: 12px;
+  font-family: inherit;
+`;
+
+const MyPlanet = styled.div`
+  padding: 110px 0px 50px 0px;
+  position: relative;
+`;
+
+const TooltipBox = styled.div`
+  position: absolute;
+  top: 30px;
+`;
+
+const Tooltip = styled.div`
+  background-color: #eef3fd;
+  border: #7689fd solid 1px;
+  border-radius: 5px;
+  color: #505bf0;
+  font-size: 12px;
+  font-weight: 500;
+  height: auto;
+  letter-spacing: -0.25px;
+  margin-top: 6.8px;
+  padding: 5px 11px;
+  position: relative;
+  width: fit-content;
+  z-index: 100;
+
+  ::after {
+    border-color: #eef3fd transparent;
+    border-style: solid;
+    border-width: 0 6px 8px 6.5px;
+    content: "";
+    display: block;
+    left: 75px;
+    position: absolute;
+    top: -7px;
+    width: 0;
+    z-index: 1;
+  }
+
+  ::before {
+    border-color: #7689fd transparent;
+    border-style: solid;
+    border-width: 0 6px 8px 6.5px;
+    content: "";
+    display: block;
+    left: 75px;
+    position: absolute;
+    top: -8px;
+    width: 0;
+    z-index: 0;
+  }
 `;
 
 //나의 레벨에 해당하는 아이콘 이미지
@@ -42,7 +139,6 @@ const Icon = styled.img`
   background-size: cover;
   background-position: center;
   border-radius: 50%;
-  border: 1px solid red;
 `;
 
 export default MyLevel;
@@ -59,38 +155,41 @@ const Progress = (props) => {
   //     }
   //   });
   return (
-    <ProgressBar>
-      {/* <HighLight width={(count / bucket_list.length) * 100 + "%"}></HighLight> */}
-      <HighLight
-        width={(my_level.point / my_level.experiencePoint) * 100 + "%"}
-      ></HighLight>
-      <Circle></Circle>
-    </ProgressBar>
+    <>
+      <ProgressBar>
+        {/* <HighLight width={(count / bucket_list.length) * 100 + "%"}></HighLight> */}
+        <HighLight
+          width={(my_level.point / my_level.experiencePoint) * 100 + "%"}
+        ></HighLight>
+      </ProgressBar>
+      <Point>
+        {my_level.point}/{my_level.experiencePoint}
+      </Point>
+    </>
   );
 };
 
 const ProgressBar = styled.div`
   background: #eee;
-  width: 100%;
-  height: 20px;
+  width: 251px;
+  height: 9px;
   border-radius: 10px;
   display: flex; // HighLight와 Circle이 한 줄에 붙어있게 하도록.
   align-items: center; //세로로 중앙 정렬
+  margin: 5px 20px 4px 48px;
 `;
 
 const HighLight = styled.div`
-  background: slateblue;
+  background: #c6c6c6;
   transition: 1s;
   width: ${(props) => props.width};
-  height: 20px;
+  height: 9px;
   border-radius: 10px;
 `;
 
-const Circle = styled.div`
-  width: 40px;
-  height: 40px;
-  background: white;
-  border: 5px solid slateblue;
-  border-radius: 40px;
-  margin: 0px 0px 0px -30px; //margin 마이너스 값을 줘서 왼쪽으로 들어가게 한다.
+const Point = styled.p`
+  margin: 4px 10px 0 0;
+  font-size: 12px;
+  text-align: right;
+  color: #999;
 `;
