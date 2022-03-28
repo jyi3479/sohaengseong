ac7663aa603bb08690fe6cb8ecd42154ab73e51a
jyi3479 <jyi3479@naver.com>
2022-03-28 11:40:57 +0900

    리포트 날짜 선택 수정 / 무한스크롤 주석

--- src/shared/InfiniteScroll.js
+++ src/shared/InfiniteScroll.js
@@ -3,20 +3,27 @@ import styled from "styled-components";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const InfinityScroll = ({ children, callNext, paging, type, isChat }) => {
+  // spinner 요소 useRef
   const spinnerRef = useRef(null);
+  // intersection observer 생성
   const handleObserver = new IntersectionObserver(([{ isIntersecting }]) => {
+    // isIntersection : 관찰 대상의 교차 상태(Boolean)
     if (isIntersecting) {
+      // 관찰 대상이 교차하면 콜백 함수 실행
       callNext();
     }
   });
 
   useEffect(() => {
+    // 다음으로 불러올 데이터 없으면 (관찰을) 실행하지 않는다.
     if (paging.next === false) return;
     if (!spinnerRef.current) return;
 
+    // spinner 요소 관찰 시작 (관찰 대상 : 스피너)
     handleObserver.observe(spinnerRef.current);
 
     return () => {
+      // 관찰하고 있던 대상 요소 관찰 중지
       spinnerRef.current && handleObserver.unobserve(spinnerRef.current);
     };
   }, [paging]);
@@ -32,7 +39,7 @@ const InfinityScroll = ({ children, callNext, paging, type, isChat }) => {
           />
         </Spinner>
       )}
-      {isChat && <>{children}</>}
+      {isChat && children}
     </>
   );
 };
