5d884a0f152ac314e62579b63a94aa003b4dea69
jyi3479 <jyi3479@naver.com>
2022-04-01 11:21:45 +0900

    다른 행성 둘러보기 예외처리

--- src/shared/apis.js
+++ src/shared/apis.js
@@ -4,7 +4,7 @@ import { getCookie } from "./cookie";
 const test_url = process.env.REACT_APP_SERVER_PORT;
 
 export const apis = axios.create({
-  aseURL: "https://sohangsung.shop", //운영 서버 주소
+  baseURL: "https://sohangsung.shop", //운영 서버 주소
   //baseURL: test_url, //테스트 서버 주소 (작업은 여기서만 합니다)
 });
 
